de97302ccbfa0414abbe4095f08f4e60678ad8a3
StefanoScolari <stefano.scolari@studio.unibo.it>
2023-02-21 16:54:59 +0100

    Cleaned and reorganized data

--- src/data/dataScraper.py
+++ src/data/dataScraper.py
@@ -1,16 +1,34 @@
 import json
 
-f = open('data.json')
+f = open('src/data/data.json')
 
 data = json.load(f)
 
-for data_entry in data:
-    print(data)
+countries = set()
 
+def getEntryCountries(data_entry):
+    countries_per_data = data_entry['clist'].split(',')
+    return [country.split(':')[1].strip() for country in countries_per_data]
+
+for data_entry in data:
+    countries_per_data = getEntryCountries(data_entry)
+    countries.update(countries_per_data)
+
+country_to_imdb_codes = {}
+
+for data_entry in data:
+    countries_per_data = getEntryCountries(data_entry)
+    for country in countries_per_data:
+        if country not in country_to_imdb_codes:
+            country_to_imdb_codes[country] = []
+        country_to_imdb_codes[country].append(data_entry['id'])
+    
  
+
+
 # Serializing json
-json_object = json.dumps(dictionary, indent=4)
+json_object = json.dumps(country_to_imdb_codes, indent=4)
  
-# Writing to sample.json
-with open("sample.json", "w") as outfile:
+# Writing to json
+with open("src/data/country_to_content.json", "w") as outfile:
     outfile.write(json_object)
